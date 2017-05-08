63014cd50120d894e74d795bb406e4f4ca71f5dc
Maurice Muschalik <maurice@actio.com.au>
2017-05-08 14:42:07 +1000

    rename

--- task/src/main/scala/Task/Task.scala
+++ task/src/main/scala/Task/Task.scala
@@ -7,13 +7,13 @@ import scala.meta._
 object Task {
 
   private lazy val tasks =  Map(
-    "TaskExtract" -> ((name: String, config: DataSet) =>
+    "extract" -> ((name: String, config: DataSet) =>
       new TaskExtract(name, config)),
-    "TaskTerm" -> ((name: String, config: DataSet) =>
+    "term" -> ((name: String, config: DataSet) =>
       new TaskTerm(name, config("term").stringOption.getOrElse("").parse[Term].get)),
-    "TaskBatch" -> ((name: String, config: DataSet) =>
+    "batch" -> ((name: String, config: DataSet) =>
       new TaskBatch(name, config)),
-    "TaskPrint" -> ((name: String, config: DataSet) =>
+    "print" -> ((name: String, config: DataSet) =>
       new TaskPrint(name)))
 
   def apply(name: String, taskType: String, config: DataSet): Task = tasks(taskType)(name, config)
