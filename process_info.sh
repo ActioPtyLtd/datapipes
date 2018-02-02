6b4bc849c5e7927808dbfca74c710f442a4a86bd
davidx <davidx@actio.com.au>
2018-02-02 12:07:02 +1100

    xxxx

--- process_info.sh
+++ process_info.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm load_datalake.conf
-./run.sh -c datalake_dsource.conf -DfileFolder=$1 -DfileName=$2.info
+./run.sh -c datalake_dsource.conf -DfileFolder=$1 -DfileName=$2
 cat datalake_repo.conf >> load_datalake.conf
-./run.sh -c load_datalake.conf -DfilePath=$1/$2.bin
+./run.sh -c load_datalake.conf -DfilePath=$3
