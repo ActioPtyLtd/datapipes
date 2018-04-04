2c68708746aefc4620121bf3dca43f28b9b33763
Maurice Muschalik <maurice@actio.com.au>
2018-04-04 17:29:34 +1000

    cleanup docker

--- process_info.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm load_datalake.conf
-./run.sh -c datalake_dsource.conf -DfileFolder=$1 -DfileName=$2.info
-cat datalake_repo.conf >> load_datalake.conf
-./run.sh -c load_datalake.conf -DfilePath=$1/$2.bin
